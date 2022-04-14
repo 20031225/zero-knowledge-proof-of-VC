0cbb62bd179be68fe54fa5a57125f5aa63fbd611
20031225 <74346357+20031225@users.noreply.github.com>
2022-04-14 15:29:08 +0800

    Update Cert_Status_SC.sol

--- income verifiable certificat/Cert_Status_SC.sol
+++ income verifiable certificat/Cert_Status_SC.sol
@@ -1,10 +1,13 @@
 pragma solidity ^0.8.1;
 
+//#################################################################
+//this contract is used to manage the status of verifiable certificate (VC)
+//which has been issued, and when the status of this contract is active,
+//the corresponding VC is valid, otherwise invalid.
+//##################################################################  
+
 contract Cert_Status_SC{
-    
-    
-    
-    
+            
   //   address IDP_income=0x544a194e6E4857927Fc88a8454dab267103e51e3;
   //   address IDP_age=0x6E819b34c53Dc81400D95ab87BFdBE3Ae80E2EA2;
     
@@ -13,9 +16,15 @@ contract Cert_Status_SC{
     
     mapping(bytes32 => bool) Cert_Status;
     
-
+//#################################################################
+//Ethereum account address of deploying this contract's authority
+//##################################################################
     address  Authority_Address=0x6E819b34c53Dc81400D95ab87BFdBE3Ae80E2EA2;
-     
+
+
+//#################################################################
+//Legal identity providers (IDPs) need to be registered through this function
+//################################################################## 
        function legal_IDP_register(address IDP_DID) public {
         
         require ( msg.sender==Authority_Address,"Your identity is illegal");
@@ -25,7 +34,9 @@ contract Cert_Status_SC{
         }
     
     
-    
+//#################################################################
+//Using this function to activate the status of VC which has been issued
+//##################################################################     
    function Set_Status_Active(bytes32 Sign_H) public {
         
         require ( legal_IDP[msg.sender]==true,"Your identity is illegal");
@@ -34,6 +45,10 @@ contract Cert_Status_SC{
         
         }
 
+
+//#################################################################
+//Using this function to revoke the status of VC which has been issued
+//################################################################## 
    function  Set_Status_Revocation(bytes32 Sign_H) public {
         
         require (legal_IDP[msg.sender]==true,"Your identity is illegal");
@@ -41,7 +56,9 @@ contract Cert_Status_SC{
         
         }       
         
-       
+ //#################################################################
+//Using this function to see whether VC is active
+//##################################################################        
     function  read_Status(bytes32 Sign_H) public view returns(bool){
         
    //     require (legal_IDP[msg.sender]==true,"Your identity is illegal");
