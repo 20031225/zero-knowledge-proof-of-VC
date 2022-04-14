4e575a6d3bcd868f88aa099e060398da70782eae
20031225 <74346357+20031225@users.noreply.github.com>
2022-04-14 09:32:09 +0800

    Update VerifySignature.sol

--- age verifiable certificate/VerifySignature.sol
+++ age verifiable certificate/VerifySignature.sol
@@ -1,9 +1,26 @@
 pragma solidity ^0.4.10;
 
+//##########################################################################################################
+//this contract is used to implement on-chain verification of the digital signature
+//of IDP, Sign(Sign_H), attached to VC to ensure that VC used to generate zero-knowledge proof (ZPK)  is issued by legal IDP. 
+//##########################################################################################################
+
 contract VerifySignature{
 
 
   function verifyByHashAndSig(bytes32 hash, bytes signature) public view returns (address){
+  
+//##########################################################################################################
+//The output of this function is the address of Ethereum account of identity provider (IDP) and through this
+//address, service provider can determine whether VC used to generate zero-knowledge proof (ZPK)  is issued by legal IDP.
+
+//The input of this function contains:
+//hash: It is the hash digest of VC, namely Sign_H and hash(Sign_H)=keccak256 (Attribute || User DID || IDP DID)
+//signature: It is the digital signature of VC and signature=sign(Sign_H). Futhermore, the algorithm of signature is ECDSA
+// and IDP can use web3.js of Ethereum to implement the signature.
+//##########################################################################################################
+  
+  
     bytes memory signedString = signature;
     uint8 v;
     
@@ -43,6 +60,9 @@ contract VerifySignature{
      bytes memory prefix = "\x19Ethereum Signed Message:\n32";
      hash = sha3(prefix, hash);
 
+//##########################################################################################################
+//ecrecover() is the verification function of signature precompiled in Ethereum
+//##########################################################################################################
      addr = ecrecover(hash, v, r, s);
   }
 }
